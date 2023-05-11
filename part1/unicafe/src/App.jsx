c6b8ac72d28194b12e72d4320ad4c71132d9a670
JustGetAClass <mohamedsalat62@gmail.com>
2023-05-11 13:08:45 +0300

    1.8 - 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,32 +1,82 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
-  const average = (good + bad * -1) / total;
-  const positive = (good * 100) / total;
+const Statistics = ({ feedback }) => {
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  const average = (feedback.good + feedback.bad * -1) / total;
+  const positive = (feedback.good * 100) / total;
+
+  if (total === 0) {
+    return <h2>No feedback given</h2>;
+  }
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" stat={feedback.good} />
+          <StatisticsLine text="neutral" stat={feedback.neutral} />
+          <StatisticsLine text="bad" stat={feedback.bad} />
+          <StatisticsLine text="all" stat={total} />
+          <StatisticsLine text="average" stat={average} />
+          <StatisticsLine text="positive" stat={positive + "%"} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const Button = ({ clickEvent, text }) => {
+  return <button onClick={clickEvent}>{text}</button>;
+};
+
+const Title = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
+const StatisticsLine = ({ text, stat }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{stat}</td>
+    </tr>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  return (
+    <div>
+      <Title text="give feedback" />
 
       <div>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad(bad + 1)}>bad</button>
+        <Button
+          clickEvent={() =>
+            setFeedback({ ...feedback, good: feedback.good + 1 })
+          }
+          text="good"
+        />
+        <Button
+          clickEvent={() =>
+            setFeedback({ ...feedback, neutral: feedback.neutral + 1 })
+          }
+          text="neutral"
+        />
+        <Button
+          clickEvent={() => setFeedback({ ...feedback, bad: feedback.bad + 1 })}
+          text="bad"
+        />
       </div>
 
-      <h1>statistics</h1>
+      <Title text="statistics" />
 
-      <h3>good {good}</h3>
-      <h3>neutral {neutral}</h3>
-      <h3>bad {bad}</h3>
-      <h3>all {total}</h3>
-      <h3>average {isNaN(average) ? 0 : average}</h3>
-      <h3>positive {isNaN(positive) ? 0 : positive}</h3>
+      <Statistics feedback={feedback} />
     </div>
   );
 };
