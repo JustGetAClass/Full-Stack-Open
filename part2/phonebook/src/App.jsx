a287c8ad5bbfa22d03f90f0534c050377bd255e8
JustGetAClass <mohamedsalat62@gmail.com>
2023-05-14 13:30:45 +0300

    2.6 - 2.8

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newObject = {
+      name: newName,
+      number: newNumber,
+    };
+    persons.forEach((person) =>
+      person.name === newObject.name
+        ? alert(`${person.name} is already to the phonebook`)
+        : setPersons(persons.concat(newObject))
+    );
+    setNewName("");
+    setNewNumber("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:&nbsp;
+          <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number:&nbsp;
+          <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
