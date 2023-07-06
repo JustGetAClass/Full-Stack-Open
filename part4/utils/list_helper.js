8dab2b0f7efe3f611ce5718a4c6baf4bd75923d0
JustGetAClass <mohamedsalat62@gmail.com>
2023-07-06 15:05:29 +0300

    4.5 test for favorite blog

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
 	return blogs.reduce((total, blog) => total + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+	let highest = 0;
+	blogs.forEach((blog) => {
+		if (blog.likes > highest) {
+			highest = blog.likes;
+		}
+	});
+
+	return blogs.find((blog) => blog.likes === highest);
+};
+
 module.exports = {
 	dummy,
-    totalLikes
+	totalLikes,
+    favoriteBlog
 };
