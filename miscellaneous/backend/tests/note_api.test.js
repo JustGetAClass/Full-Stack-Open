aa41e5336d9096d683e51f9787e1cc2e951e1e6a
JustGetAClass <mohamedsalat62@gmail.com>
2023-07-13 14:56:27 +0300

    using beforeEach jest function

--- miscellaneous/backend/tests/note_api.test.js
+++ miscellaneous/backend/tests/note_api.test.js
@@ -3,6 +3,26 @@ const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+	{
+		content: "HTML is easy",
+		important: false,
+	},
+	{
+		content: "Browser can execute only JavaScript",
+		important: true,
+	},
+];
+
+beforeEach(async () => {
+	await Note.deleteMany({});
+	let noteObject = new Note(initialNotes[0]);
+	await noteObject.save();
+	noteObject = new Note(initialNotes[1]);
+	await noteObject.save();
+});
 
 test("notes are returned as json", async () => {
 	await api
