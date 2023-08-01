5a41ce45ccad47345ccd94e7b0907b5b2a03e9f8
JustGetAClass <mohamedsalat62@gmail.com>
2023-08-01 10:16:33 +0300

    refactored router to use async/await

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
+blogsRouter.get("/", async (request, response) => {
+	const blogs = await Blog.find({});
+	response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
+blogsRouter.post("/", async (request, response) => {
 	const body = request.body;
 	const blog = new Blog({
 		title: body.title,
@@ -16,13 +15,8 @@ blogsRouter.post("/", (request, response) => {
 		likes: body.likes,
 	});
 
-	blog.save()
-		.then((result) => {
-			response.status(201).json(result);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	const result = await blog.save();
+	response.status(201).json(result);
 });
 
 module.exports = blogsRouter;
