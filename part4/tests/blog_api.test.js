7107a8bfb721a21393d13fa9002ccaa7c65cd9c5
JustGetAClass <mohamedsalat62@gmail.com>
2023-08-01 10:04:04 +0300

    creating blog api test

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,11 +1,22 @@
 const supertest = require("supertest");
 const mongoose = require("mongoose");
 
+const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-test("all blogs are returned", async() => {
-    
-})
+beforeEach(async () => {
+	await Blog.deleteMany({});
+	await Blog.insertMany(helper.blogs);
+}, 100000);
+
+test("all blogs are returned", async () => {
+	const response = await api.get("/api/blogs");
+	expect(response.body).toHaveLength(helper.blogs.length);
+});
+
+afterAll(async () => {
+    await mongoose.connection.close()
+  })
