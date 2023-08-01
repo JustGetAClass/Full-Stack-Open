6cd52810ccd539cbcb5509a412966a7b95a1c103
JustGetAClass <mohamedsalat62@gmail.com>
2023-08-01 11:35:33 +0300

    4.10 test added blog

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -30,6 +30,27 @@ test("blogs have an id", async () => {
 	expect(response.body[0].id).toBeDefined();
 });
 
+test("a blog can be added", async () => {
+	const newBlog = {
+		title: "Blog Exercise",
+		author: "Blog Author",
+		url: "Blog Link",
+		likes: 1,
+	};
+
+	await api
+		.post("/api/blogs")
+		.send(newBlog)
+		.expect(201)
+		.expect("Content-Type", /application\/json/);
+
+	const blogsAtEnd = await helper.blogsInDb();
+	expect(blogsAtEnd).toHaveLength(helper.blogs.length + 1);
+
+	const titles = blogsAtEnd.map((blog) => blog.title);
+	expect(titles).toContain("Blog Exercise");
+});
+
 afterAll(async () => {
 	await mongoose.connection.close();
 });
