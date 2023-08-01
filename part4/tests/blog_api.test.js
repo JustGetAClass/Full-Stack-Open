fc0f664a54b6d4454807b956bf344f16830f9a5b
JustGetAClass <mohamedsalat62@gmail.com>
2023-08-01 09:41:46 +0300

    configuring node modes and test server

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,11 @@
+const supertest = require("supertest");
+const mongoose = require("mongoose");
+
+const app = require("../app");
+const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+test("all blogs are returned", async() => {
+    
+})
